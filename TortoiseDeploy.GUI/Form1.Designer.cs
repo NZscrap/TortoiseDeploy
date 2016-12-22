aa00aed3bf2d520bab140c6930dd40b125e95cce
Mitch <mitchreidnz@gmail.com>
2016-12-22 21:46:00 +1300

    Added "select all/none" button Fixed checkboxlist so that it can display the changed files, and where they will deploy to

--- TortoiseDeploy.GUI/Form1.Designer.cs
+++ TortoiseDeploy.GUI/Form1.Designer.cs
@@ -25,19 +25,29 @@
 		private void InitializeComponent() {
 			this.checkListChangedPaths = new System.Windows.Forms.CheckedListBox();
 			this.btnMerge = new System.Windows.Forms.Button();
+			this.chkBoxSelectAll = new System.Windows.Forms.CheckBox();
+			this.btnDeploy = new System.Windows.Forms.Button();
+			this.txtLog = new System.Windows.Forms.RichTextBox();
 			this.SuspendLayout();
 			// 
 			// checkListChangedPaths
 			// 
 			this.checkListChangedPaths.FormattingEnabled = true;
-			this.checkListChangedPaths.Location = new System.Drawing.Point(12, 42);
+			this.checkListChangedPaths.Items.AddRange(new object[] {
+            "Country Road UAT\\Website\\Xslt\\Default.xslt\t->\t\\\\crvweb6.estaronline.local\\Country" +
+                " Road UAT\\Website\\Xslt\\Default.xslt",
+            "Country Road UAT\\Website\\Xslt\\Cart.xslt\t\t->\t\\\\crvweb6.estaronline.local\\Country R" +
+                "oad UAT\\Website\\Xslt\\Cart.xslt",
+            "Country Road UAT\\Website\\Assets\\cr_scripts.js \t->\t\\\\crvweb6.estaronline.local\\Cou" +
+                "ntry Road UAT\\Website\\Assets\\cr_scripts.js"});
+			this.checkListChangedPaths.Location = new System.Drawing.Point(12, 12);
 			this.checkListChangedPaths.Name = "checkListChangedPaths";
-			this.checkListChangedPaths.Size = new System.Drawing.Size(333, 559);
+			this.checkListChangedPaths.Size = new System.Drawing.Size(769, 259);
 			this.checkListChangedPaths.TabIndex = 0;
 			// 
 			// btnMerge
 			// 
-			this.btnMerge.Location = new System.Drawing.Point(382, 54);
+			this.btnMerge.Location = new System.Drawing.Point(12, 300);
 			this.btnMerge.Name = "btnMerge";
 			this.btnMerge.Size = new System.Drawing.Size(224, 46);
 			this.btnMerge.TabIndex = 1;
@@ -45,17 +55,51 @@
 			this.btnMerge.UseVisualStyleBackColor = true;
 			this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
 			// 
+			// chkBoxSelectAll
+			// 
+			this.chkBoxSelectAll.AutoSize = true;
+			this.chkBoxSelectAll.Location = new System.Drawing.Point(12, 277);
+			this.chkBoxSelectAll.Name = "chkBoxSelectAll";
+			this.chkBoxSelectAll.Size = new System.Drawing.Size(107, 17);
+			this.chkBoxSelectAll.TabIndex = 2;
+			this.chkBoxSelectAll.Text = "Select All / None";
+			this.chkBoxSelectAll.UseVisualStyleBackColor = true;
+			this.chkBoxSelectAll.CheckedChanged += new System.EventHandler(this.chkBoxSelectAll_CheckedChanged);
+			// 
+			// btnDeploy
+			// 
+			this.btnDeploy.Location = new System.Drawing.Point(242, 300);
+			this.btnDeploy.Name = "btnDeploy";
+			this.btnDeploy.Size = new System.Drawing.Size(224, 46);
+			this.btnDeploy.TabIndex = 3;
+			this.btnDeploy.Text = "Deploy Selected";
+			this.btnDeploy.UseVisualStyleBackColor = true;
+			this.btnDeploy.Click += new System.EventHandler(this.btnDeploy_Click);
+			// 
+			// txtLog
+			// 
+			this.txtLog.Location = new System.Drawing.Point(12, 352);
+			this.txtLog.Name = "txtLog";
+			this.txtLog.ReadOnly = true;
+			this.txtLog.Size = new System.Drawing.Size(769, 242);
+			this.txtLog.TabIndex = 4;
+			this.txtLog.Text = "";
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(864, 606);
+			this.ClientSize = new System.Drawing.Size(794, 606);
+			this.Controls.Add(this.txtLog);
+			this.Controls.Add(this.btnDeploy);
+			this.Controls.Add(this.chkBoxSelectAll);
 			this.Controls.Add(this.btnMerge);
 			this.Controls.Add(this.checkListChangedPaths);
 			this.Name = "Form1";
 			this.Text = "TortoiseDeploy";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -63,6 +107,9 @@
 
 		private System.Windows.Forms.CheckedListBox checkListChangedPaths;
 		private System.Windows.Forms.Button btnMerge;
+		private System.Windows.Forms.CheckBox chkBoxSelectAll;
+		private System.Windows.Forms.Button btnDeploy;
+		private System.Windows.Forms.RichTextBox txtLog;
 	}
 }
 
--- TortoiseDeploy.GUI/Form1.cs
+++ TortoiseDeploy.GUI/Form1.cs
@@ -7,20 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TortoiseDeploy;
 
 namespace TortoiseDeploy.GUI {
 	public partial class Form1 : Form {
+
+		TortoiseDeploy deployer;
+		List<string> changedPaths;
+
 		public Form1() {
 			InitializeComponent();
 		}
 
 		private void Form1_Load(object sender, EventArgs e) {
-			List<string> changedPaths = new List<String>();
 
-			changedPaths.Add("Test one");
-			changedPaths.Add("Test two");
+			// Create our deployer instance so that we can merge/deploy the files
+			deployer = new TortoiseDeploy();
 
-			((ListBox)this.checkListChangedPaths).DataSource = changedPaths;
+			changedPaths = new List<String>();
+			changedPaths.Add("C:\\Users\\mitch\\Desktop\\local tortoiseDeploy tests\\other test.txt");
+			changedPaths.Add("C:\\Users\\mitch\\Desktop\\local tortoiseDeploy tests\\test.txt");
+
+			// Identify the longest display name in our list of changed paths
+			int maxPathLength = changedPaths.Max(s => deployer.GetDisplayName(s).Length);
+
+			// Create a source => destination display string for each file
+			List<string> display = new List<string>();
+			foreach(string path in changedPaths) {
+				string displayPath = deployer.GetDisplayName(path);
+				// Pad the displayname up to the longest one, so that our => symbol shows consistently
+				displayPath += new string(' ', Math.Abs(displayPath.Length - maxPathLength));
+				display.Add(displayPath + "\t=>\t" + deployer.GetDeploymentTarget(path));
+			}
+
+			// Set the list of files that changed
+			((ListBox)this.checkListChangedPaths).DataSource = display;
+
+			OverwriteLog(deployer.Log);
+		}
+
+		public void OverwriteLog(string log) {
+			this.txtLog.Text = log;
 		}
 
 		private void btnMerge_Click(object sender, EventArgs e) {
@@ -31,6 +58,26 @@ namespace TortoiseDeploy.GUI {
 			}
 
 			MessageBox.Show(output.ToString());
+
+			// Update the log output
+			OverwriteLog(deployer.Log);
+		}
+
+		private void chkBoxSelectAll_CheckedChanged(object sender, EventArgs e) {
+			if (this.chkBoxSelectAll.Checked) {
+				for(int i = 0; i < this.checkListChangedPaths.Items.Count; i++) {
+					this.checkListChangedPaths.SetItemChecked(i, true);
+				}
+			} else {
+				for (int i = 0; i < this.checkListChangedPaths.Items.Count; i++) {
+					this.checkListChangedPaths.SetItemChecked(i, false);
+				}
+			}
+		}
+
+		private void btnDeploy_Click(object sender, EventArgs e) {
+			// Update the log output
+			OverwriteLog(deployer.Log);
 		}
 	}
 }
