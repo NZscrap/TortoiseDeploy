a3526400a9d6e0f209c6f66b3228d0e3a308af20
Mitch <mitchreidnz@gmail.com>
2016-12-23 20:50:23 +1300

    Set up commandline argument handling, so that the GUI can be launched from TortoiseSVN. Added documentation to the GUI methods

--- TortoiseDeploy.GUI/Form1.cs
+++ TortoiseDeploy.GUI/Form1.cs
@@ -16,7 +16,9 @@ namespace TortoiseDeploy.GUI {
 		List<string> changedPaths;
 		List<DeploymentMapping> deploymentMappings;
 
-		public Form1() {
+		public Form1(List<String> changedPaths) {
+			this.changedPaths = changedPaths;
+
 			InitializeComponent();
 		}
 
@@ -24,10 +26,6 @@ namespace TortoiseDeploy.GUI {
 
 			// Create our deployer instance so that we can merge/deploy the files
 			deployer = new TortoiseDeploy();
-
-			changedPaths = new List<String>();
-			changedPaths.Add("C:\\Users\\mitch\\Desktop\\local tortoiseDeploy tests\\other test.txt");
-			changedPaths.Add("C:\\Users\\mitch\\Desktop\\local tortoiseDeploy tests\\test.txt");
 
 			// Identify the longest display name in our list of changed paths
 			int maxPathLength = changedPaths.Max(s => deployer.GetDisplayName(s).Length);
@@ -48,10 +46,18 @@ namespace TortoiseDeploy.GUI {
 			OverwriteLog(deployer.Log);
 		}
 
-		public void OverwriteLog(string log) {
+		/// <summary>
+		/// Replace the text in the log output text box
+		/// </summary>
+		/// <param name="log">Text to display</param>
+		private void OverwriteLog(string log) {
 			this.txtLog.Text = log;
 		}
 
+		/// <summary>
+		/// Get the list of DeploymentMapping objects representing all of the selected files
+		/// </summary>
+		/// <returns>List of selected DeploymentMapping items</returns>
 		private List<DeploymentMapping> getSelected() {
 			List<DeploymentMapping> returnVal = new List<DeploymentMapping>();
 
@@ -62,7 +68,15 @@ namespace TortoiseDeploy.GUI {
 			return returnVal;
 		}
 
+		/// <summary>
+		/// Handle the click event of the Merge button.
+		/// This uses the deployer to launch the configured merge tool for all selected files.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void btnMerge_Click(object sender, EventArgs e) {
+			
+			// Loop through each of the selected files individually and launch the merge tool
 			foreach(var i in getSelected()) {
 				deployer.Merge(i.Source, deployer.GetDeploymentTarget(i.Source));
 			}
@@ -71,7 +85,15 @@ namespace TortoiseDeploy.GUI {
 			OverwriteLog(deployer.Log);
 		}
 
+		/// <summary>
+		/// Handle the click event of the Deploy button.
+		/// This uses the deployer to deploy all the selected files
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void btnDeploy_Click(object sender, EventArgs e) {
+			
+			// Loop through all of our selected files, and deploy them one at a time.
 			foreach (var i in getSelected()) {
 				deployer.Deploy(i.Source, deployer.GetDeploymentTarget(i.Source));
 			}
@@ -80,6 +102,12 @@ namespace TortoiseDeploy.GUI {
 			OverwriteLog(deployer.Log);
 		}
 
+		/// <summary>
+		/// Handle click events on the "Select All / None" checkbox.
+		/// This will select / deselect all of the files in the list.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void chkBoxSelectAll_CheckedChanged(object sender, EventArgs e) {
 			if (this.chkBoxSelectAll.Checked) {
 				for(int i = 0; i < this.checkListChangedPaths.Items.Count; i++) {
@@ -92,6 +120,12 @@ namespace TortoiseDeploy.GUI {
 			}
 		}
 
+		/// <summary>
+		/// Handle clicks on links in the rich text box.
+		/// When a link is clicked, we launch the users default browser to navigate
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e) {
 			System.Diagnostics.Process.Start(e.LinkText.ToString());
 		}
--- TortoiseDeploy.GUI/Program.cs
+++ TortoiseDeploy.GUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,10 +11,24 @@ namespace TortoiseDeploy.GUI {
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main() {
+		static void Main(string[] args) {
+
+			// Load up the list of files that changed
+			List<string> changedFiles = new List<string>();
+			using (StreamReader reader = new StreamReader(args[0])) {
+				while (!reader.EndOfStream) {
+					string changedFile = reader.ReadLine();
+					// TortoiseSVN gives us the changed path linux-style (ie C:/folder/path/to/file.txt)
+					// We want to get the windows style (ie, C:\\folder\\path\\to\\file.txt)
+					// We're going to consistently deal in windows style paths
+					changedFile = Path.GetFullPath(changedFile);
+					changedFiles.Add(changedFile);
+				}
+			}
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new Form1(changedFiles));
 		}
 	}
 }
